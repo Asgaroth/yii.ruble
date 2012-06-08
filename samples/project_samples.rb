7d505f6f1d85c770c4b200b9b89463c58a64b165
Alex Urbano <asgaroth.belem@gmail.com>
2012-06-07 23:15:30 -0500

    Added base for sample project

--- samples/project_samples.rb
+++ samples/project_samples.rb
@@ -1,9 +1,10 @@
 require 'ruble'
 
-project_sample "Project Sample Example" do |s|
-    s.id = "sample.local"
+project_sample "Yii Base Project" do |s|
+    s.id = "yii.base"
     s.category = "com.aptana.projects.samples.web.category"
     s.location = "samples/local_sample.zip"
-    s.description = "A basic project sample"
-    s.natures = ["com.aptana.projects.webnature"]
+    s.description = "Base Preconfigured Yii Project"
+    s.natures = ["com.aptana.projects.webnature", "com.aptana.editor.php.phpNature"]
+    #s.icon = ["samples/yii.png"]
 end

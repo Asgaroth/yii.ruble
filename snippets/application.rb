65bd067f964260bda47ed4f290e01e9a5e1e4d07
Alex Urbano <asgaroth.belem@gmail.com>
2012-06-28 01:18:44 -0500

    fixed... something :S

--- snippets/application.rb
+++ snippets/application.rb
@@ -1,9 +1,9 @@
 require 'ruble'
 
 with_defaults :scope => 'source' do
-	
+
 	snippet_category "application" do |category|
-	  category.icon_path = "icons/yii.png"
+		category.icon_path = "icons/yii.png"
 	end
 
 	#createUrl
@@ -14,12 +14,12 @@ with_defaults :scope => 'source' do
 		snip.category = "Creates an Application Url"
 		snip.icon_path = "icons/icons.png"
 	end
-	
+
 	#createUrl
-  snippet "Base Url" do |snip|
-    snip.trigger = "baseurl"
-    snip.expansion = "Yii::app()->baseUrl"
-  end
+	snippet "Base Url" do |snip|
+		snip.trigger = "baseurl"
+		snip.expansion = "Yii::app()->baseUrl"
+	end
 
 	snippet "Application Params" do |snip|
 		snip.trigger = "params"
@@ -30,11 +30,11 @@ with_defaults :scope => 'source' do
 		snip.trigger = "get"
 		snip.expansion = "Yii::app()->request->getParam('${1:route}', '${1:default}')"
 	end
-	
+
 	snippet "Application" do |snip|
-    snip.trigger = "app"
-    snip.expansion = "Yii::app()->${}"
-  end
+		snip.trigger = "app"
+		snip.expansion = "Yii::app()->${}"
+	end
 
 	snippet "Request Post" do |snip|
 		snip.trigger = "get"
@@ -59,29 +59,29 @@ try{
 }
 '
 	end
-	
+
 	snippet 'Yii User' do |s|
-	  s.trigger = 'yiiuser'
-	  s.expansion = "Yii::app()->user${1}"
+		s.trigger = 'yiiuser'
+		s.expansion = "Yii::app()->user${1}"
 	end
-	
+
 	snippet 'Set Flash Message' do |s|
-	  s.trigger = 'yf'
-	  s.expansion = "Yii::app()->user->setFlash('${1:key}', Yii::t('${2:app}', '${3:message}'));"
+		s.trigger = 'yf'
+		s.expansion = "Yii::app()->user->setFlash('${1:key}', Yii::t('${2:app}', '${3:message}'));"
 	end
-	
+
 	snippet 'Get Flash Message' do |s|
-	  s.trigger = 'yf'
-	  s.expansion = "Yii::app()->user->getFlash('${1:key}')"
+		s.trigger = 'yf'
+		s.expansion = "Yii::app()->user->getFlash('${1:key}')"
 	end
-	
+
 	snippet 'Translate' do |s|
-	  s.trigger = 'yiit'
-	  s.expansion = "Yii::t('${1:app}', '${2:message}')"
+		s.trigger = 'yiit'
+		s.expansion = "Yii::t('${1:app}', '${2:message}')"
 	end
-	
+
 	snippet 'Application Log' do |s|
-    s.trigger = 'log'
-    s.expansion = "Yii::log(${1:msg},CLogger::LEVEL_${2:INFO}, '${3:application}')"
-  end
+		s.trigger = 'log'
+		s.expansion = "Yii::log(${1:msg},CLogger::LEVEL_${2:INFO}, '${3:application}')"
+	end
 end
